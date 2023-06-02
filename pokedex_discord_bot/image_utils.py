56e5b6083b7038118b474d75bd0f21e867ee0263
AlberLC <37489786+AlberLC@users.noreply.github.com>
2023-06-02 07:37:44 +0200

    Add files via upload

---
+++ pokedex_discord_bot/image_utils.py
@@ -0,0 +1,68 @@
+import pathlib
+from collections.abc import Iterable, Sequence
+
+import cv2
+import numpy
+
+
+def collection(files: Iterable[str | pathlib.Path | bytes], obtained: Sequence[int] | set[int]) -> bytes:
+    interrogation_image = pathlib.Path('images/interrogation.png').read_bytes()
+
+    files = list(files)
+    size = get_size(files[0])
+    if size != (475, 475):
+        interrogation_image = resize(interrogation_image, size)
+
+    return concatenate(
+        (file if i in obtained else interrogation_image for i, file in enumerate(files, start=1)),
+        images_per_line=14
+    )
+
+
+def concatenate(
+    files: Iterable[str | pathlib.Path | bytes],
+    images_per_line,
+    resize_to: tuple[int, int] = None
+) -> bytes:
+    if resize_to:
+        def resize_image(image_: numpy.ndarray, resize_to_: tuple[int, int]) -> numpy.ndarray:
+            return cv2.resize(image_, resize_to_, interpolation=cv2.INTER_AREA)
+    else:
+        def resize_image(image_: numpy.ndarray, _) -> numpy.ndarray:
+            return image_
+
+    column_images = []
+    row_images = []
+    for i, file in enumerate(files, start=1):
+        column_images.append(resize_image(to_numpy_array(file), resize_to))
+        if i % images_per_line == 0:
+            row_images.append(cv2.hconcat(column_images))
+            column_images.clear()
+
+    if column_images:
+        last_row_image = cv2.hconcat(column_images)
+        last_row_image = cv2.copyMakeBorder(last_row_image, 0, 0, 0, (images_per_line - len(column_images)) * last_row_image.shape[0], cv2.BORDER_CONSTANT)
+        row_images.append(last_row_image)
+    final_image = cv2.vconcat(row_images)
+    return cv2.imencode('.png', final_image, [cv2.IMWRITE_PNG_COMPRESSION, 3])[1].tostring()
+
+
+def get_size(file: str | pathlib.Path | bytes) -> tuple[int, int]:
+    image = to_numpy_array(file)
+    return image.shape[0], image.shape[1]
+
+
+def resize(file: str | pathlib.Path | bytes, resize_to: tuple[int, int]) -> bytes:
+    resized_image = cv2.resize(to_numpy_array(file), resize_to, interpolation=cv2.INTER_AREA)
+    return cv2.imencode('.png', resized_image)[1].tostring()
+
+
+def to_numpy_array(file: str | pathlib.Path | bytes, ) -> numpy.ndarray:
+    if isinstance(file, str):
+        return cv2.imread(file, cv2.IMREAD_UNCHANGED)
+    elif isinstance(file, pathlib.Path):
+        return cv2.imread(str(file), cv2.IMREAD_UNCHANGED)
+    else:
+        # noinspection PyTypeChecker
+        array = numpy.fromstring(file, numpy.uint8)
+        return cv2.imdecode(array, cv2.IMREAD_UNCHANGED)
---
+++ pokedex_discord_bot/main.py
@@ -0,0 +1,103 @@
+import asyncio
+import io
+import random
+from collections import defaultdict
+
+import discord
+import requests
+
+import image_utils
+
+WILD_POKEMON_COLOR = (200, 150, 50)
+POKEDEX_COLOR = (200, 50, 50)
+POKEMON_EVERY_SECONDS = 20
+N_POKEMONS = 151
+RESIZE_TO = (40, 40)
+
+
+def load_images():
+    pokemon_id = 0  # for final print if N_POKEMONS <= 0
+
+    for pokemon_id in range(1, N_POKEMONS + 1):
+        response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}/')
+        data = response.json()
+        if 'mime' in data['name']:
+            name = 'Mr. Mime'
+        else:
+            name = data['name'].title()
+        image_url = data['sprites']['other']['official-artwork']['front_default']
+        image = requests.get(image_url).content
+        pokemons.append({'id': data['id'], 'name': name, 'image': image, 'thumbnail': image_utils.resize(image, RESIZE_TO)})
+        if pokemon_id % 10 == 0:
+            print(f'{pokemon_id} pokémons cargados.')
+
+    print(f'{pokemon_id} pokémons cargados.')
+
+
+async def run_pokegame(message: discord.Message):
+    while True:
+        current_pokemons[message.channel.id] = random.choice(pokemons)
+        embed = discord.Embed(
+            title="¡Un pokémon salvaje apareció!",
+            description='¡Escribe su nombre para capturarlo!',
+            color=discord.Color.from_rgb(*WILD_POKEMON_COLOR)
+        )
+        thumbnail_file = discord.File("images/pokeball.png", filename="pokeball.png")
+        embed.set_thumbnail(url='attachment://pokeball.png')
+        image_file = discord.File(fp=io.BytesIO(current_pokemons[message.channel.id]['image']), filename='pokemon.png')
+        embed.set_image(url='attachment://pokemon.png')
+        await message.channel.send(files=[thumbnail_file, image_file], embed=embed)
+
+        await asyncio.sleep(POKEMON_EVERY_SECONDS)
+
+
+async def show_pokedex(message: discord.Message):
+    caught_pokemon_ids = users_pokedex[message.author.id]
+    embed = discord.Embed(
+        title=f"Pokédex de {message.author.name}.",
+        description=f"Has atrapado {len(caught_pokemon_ids)} pokémons.",
+        color=discord.Color.from_rgb(*POKEDEX_COLOR)
+    )
+    thumbnail_file = discord.File("images/pokedex.png", filename="pokedex.png")
+    embed.set_thumbnail(url='attachment://pokedex.png')
+    images = (pokemon['image'] for pokemon in pokemons)
+    image_file = discord.File(fp=io.BytesIO(image_utils.collection(images, caught_pokemon_ids)), filename='caught_pokemons.png')
+    embed.set_image(url='attachment://caught_pokemons.png')
+
+    await message.channel.send(files=[thumbnail_file, image_file], embed=embed)
+
+
+pokemons: list[dict] = []
+current_pokemons: defaultdict[int, dict] = defaultdict(dict)
+users_pokedex: defaultdict[int, set[int]] = defaultdict(set)
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} activado (id: {client.user.id}).')
+
+
+@client.event
+async def on_message(message: discord.Message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('/start'):
+        asyncio.create_task(run_pokegame(message))
+        return
+
+    if message.content.startswith('/pokedex'):
+        await show_pokedex(message)
+
+    current_pokemon = current_pokemons[message.channel.id]
+    if current_pokemon and current_pokemon['name'].lower() == message.content.strip().lower():
+        current_pokemons[message.channel.id] = {}
+        await message.channel.send(f"¡{message.author.name} ha capturado un {current_pokemon['name']}!")
+        users_pokedex[message.author.id].add(current_pokemon['id'])
+
+
+load_images()
+client.run('bot token')

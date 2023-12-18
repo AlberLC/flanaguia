f2dd76d0c9d30fbc6b3050874a9c58ab9d590e6e
AlberLC <alberlc@outlook.com>
2023-12-18 22:21:58 +0100

    Add apis

--- apis/trivial_mini/questions_to_excel.py
+++ apis/trivial_mini/questions_to_excel.py
@@ -1,12 +1,11 @@
 import asyncio
-import itertools
 
 import openpyxl
 
 from models.question import find_questions
 
 FILE_PATH = 'KahootQuizTemplate.xlsx'
-AUTHORS = []
+AUTHORS = ['juan']
 SECONDS_LIMIT = 60
 
 STATEMENT_COLUMN = 'B'
@@ -24,12 +23,12 @@ async def main():
     current_row = INITIAL_ROW
     for question in await find_questions():
         correct_answers = [str(i) for i, answer in enumerate(question.answers, start=1) if answer.is_correct]
-        if question.author not in AUTHORS or not correct_answers:
+        if question.author not in AUTHORS or len(question.answers) < 2 or not correct_answers:
             continue
 
         worksheet[f'{STATEMENT_COLUMN}{current_row}'] = f'{question.author} pregunta: {question.statement}'
-        for column, answer in itertools.zip_longest(ANSWER_COLUMNS, question.answers):
-            worksheet[f'{column}{current_row}'] = answer.text if answer else None
+        for column, answer in zip(ANSWER_COLUMNS, question.answers):
+            worksheet[f'{column}{current_row}'] = answer.text
         worksheet[f'{TIME_COLUMN}{current_row}'] = SECONDS_LIMIT
         worksheet[f'{CORRECT_COLUMN}{current_row}'] = ','.join(correct_answers)
 

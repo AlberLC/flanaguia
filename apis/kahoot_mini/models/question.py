9bed5bdefdaf0cf69021963ef8681519932a28a3
AlberLC <alberlc@outlook.com>
2024-02-14 08:37:58 +0100

    Add question validations

--- apis/kahoot_mini/models/question.py
+++ apis/kahoot_mini/models/question.py
@@ -1,10 +1,14 @@
-from typing import Annotated
+from typing import Annotated, Any
 
-from pydantic import BaseModel, BeforeValidator, Field
+from pydantic import BaseModel, BeforeValidator, Field, model_validator
 from pymongo import ReturnDocument
 
 from database import create_object_id, question_collection
 from models.answer import Answer
+
+ANSWER_MAX = 75
+STATEMENT_MAX = 120
+TEMPLATE_LENGTH = 11
 
 
 class Question(BaseModel):
@@ -12,6 +16,47 @@ class Question(BaseModel):
     author: str
     statement: str
     answers: list[Answer] = []
+
+    @model_validator(mode='before')
+    @classmethod
+    def validate(cls, data: Any) -> dict:
+        if not isinstance(data, dict):
+            raise ValueError('data must be dict')
+
+        if {*data, 'id', '_id'} != {*cls.model_fields, '_id'}:
+            raise ValueError('wrong question format')
+
+        data['author'] = data['author'].strip()
+        data['statement'] = data['statement'].strip()
+
+        if not data['author']:
+            raise ValueError('no author')
+
+        if not data['statement']:
+            raise ValueError('no statement')
+
+        if len(data['author']) + TEMPLATE_LENGTH + len(data['statement']) > STATEMENT_MAX:
+            raise ValueError('author and/or statement too long')
+
+        if not 2 <= len(data['answers']) <= 4:
+            raise ValueError('wrong number of answers')
+
+        correct_answers = 0
+        for answer_data in data['answers']:
+            if {*answer_data, 'is_correct'} != set(Answer.model_fields):
+                raise ValueError('wrong answer format')
+
+            answer_data['text'] = answer_data['text'].strip()
+            if not 0 < len(answer_data['text']) <= ANSWER_MAX:
+                raise ValueError('wrong answer length')
+
+            if answer_data.get('is_correct', False):
+                correct_answers += 1
+
+        if not correct_answers:
+            raise ValueError('no correct answers')
+
+        return data
 
 
 async def delete_question(id: str) -> None:
